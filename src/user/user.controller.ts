a52e02708b8a73df94fc44678de9140e464bdc41
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-12-27 21:10:33 +0900

    Feat: UserController, UserService 임시 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { UserService } from './user.service';
+import { RegisterRequestDto } from './dto/register.request.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUser(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Post()
+  register(@Body() data: RegisterRequestDto) {
+    this.userService.register(data.email, data.nickname, data.password);
+  }
+
+  @Post('login')
+  login() {}
+
+  @Post('logout')
+  logout(@Req() req: Request, @Res() res: Response) {
+    req.logout((err) => err ?? console.error(err));
+    res.clearCookie('connect.sid', { httpOnly: true });
+    res.send('ok');
+  }
+}
