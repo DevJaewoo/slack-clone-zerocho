5677cd597c9d59b24543793dde1ddee7af8a13fb
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-12-27 21:45:04 +0900

    Feat: Passport, Swagger 구성

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sleact API')
+    .setDescription('Sleact API for Development')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(cookieParser());
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   const port = process.env.port ?? 3000;
   await app.listen(port);
   console.log(`Listening on port ${port}`);
