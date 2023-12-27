b1413c40aaa46bdf64d398e4245919a8638c8e1a
Jaewoo Ahn <DevJaewoo@gmail.com>
2023-12-27 21:10:48 +0900

    Feat: Channel, Workspace, DM Module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UserModule } from './user/user.module';
+import { DmModule } from './dm/dm.module';
+import { ChannelModule } from './channel/channel.module';
+import { WorkspaceModule } from './workspace/workspace.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, DmModule, ChannelModule, WorkspaceModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
