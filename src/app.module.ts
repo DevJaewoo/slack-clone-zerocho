d230750e9d69fcec29c740650a0d8fa9963df1b5
Jaewoo Ahn <DevJaewoo@gmail.com>
2024-01-08 21:09:02 +0900

    Feat: TypeORM Entity 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,46 @@ import { UserModule } from './user/user.module';
 import { DmModule } from './dm/dm.module';
 import { ChannelModule } from './channel/channel.module';
 import { WorkspaceModule } from './workspace/workspace.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelChats } from './entities/ChannelChats';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
+import { DMs } from './entities/DMs';
+import { Mentions } from './entities/Mentions';
+import { Users } from './entities/Users';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Workspaces } from './entities/Workspaces';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, DmModule, ChannelModule, WorkspaceModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UserModule,
+    DmModule,
+    ChannelModule,
+    WorkspaceModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [
+        ChannelChats,
+        ChannelMembers,
+        Channels,
+        DMs,
+        Mentions,
+        Users,
+        WorkspaceMembers,
+        Workspaces,
+      ],
+      synchronize: false,
+      logging: true,
+      charset: 'utf8mb4',
+      keepConnectionAlive: true, // Hot Reloading 시 Connection을 끊었다 다시 연결하는 현상 방지
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
